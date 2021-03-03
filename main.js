e6f0255120df114c7efcb073660a0def3903c75c
ArtidSun <sun_vijitpanmai@hotmail.com>
2021-03-03 12:25:48 +0700

    Update Date of Birth, Add picture

--- main.js
+++ main.js
@@ -1,14 +1,95 @@
+const constraints = {
+    firstname: {
+        presence: true,
+    },
+    lastname: {
+        presence: true,
+    },
+    age: {
+        presence: true,
+        numericality: {
+            lessThan: 150
+        }
+    },
+    gender: {
+        presence: true,
+    },
+    email: {
+        presence: true,
+        email: true
+    },
+    phone: {
+        presence: true,
+        length: {
+            minimum: 10,
+            message: "must be at least 10 digits"
+        }
+    },
+    day: {
+        presence: true,
+        numericality: {
+            lessThan: 32
+        }
+    },
+    month: {
+        presence: true,
+        numericality: {
+            lessThan: 13
+        }
+    },
+    year: {
+        presence: true,
+        numericality: {
+            lessThan: 2021
+        }
+    }
+}
+
+
 const app = Vue.createApp({
     data() {
         return {
-            
+            firstname: null,
+            lastname: null,
+            age: null,
+            gender: null,
+            email: null,
+            phone: null,
+            errors: null,
+            day: null,
+            month: null,
+            year: null,
+            submitComple: false,
+            img:"images/Me.jpg"
         }
     },
     methods: {
-
-    },
-    computed: {
-        
+        checkForm() {
+            this.errors = validate({
+                firstname: this.firstname,
+                lastname: this.lastname,
+                age: this.age,
+                gender: this.gender,
+                email: this.email,
+                phone: this.phone,
+                day: this.day,
+                month: this.month,
+                year: this.year
+            },
+                constraints)
+            if (!this.errors) {
+                alert("Registered successfully.")
+                this.submitComple = true;
+            } else {
+                alert("Your profile is not update, please check your profile")
+            }
+        },
+        submitForm() {
+            if (this.submitComple) {
+                alert("Your profile is updated successfully.")
+            }
+        }
     }
-
 })
+
+app.mount('#app')
